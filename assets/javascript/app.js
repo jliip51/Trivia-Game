2077b846331ddb82861a637ed701a6059231a0d9
jliip51 <jliip51@gmail.com>
2017-05-29 19:44:54 -0400

    update v2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,18 +7,18 @@ var noAnswer = 0;
 var questionindex = 0;
 var intervalId;
 var quiz = {
-	question: ['1stQuestion', '2ndQuestion', '3rdQuestion'],
-	answer: ['1stChoiceC', '2ndChoiceD', '3rdChoiceA'],
-	choice1: ['1stChoiceA', '2ndChoiceA', '3rdChoiceA'],
-	choice2: ['1stChoiceB', '2ndChoiceB', '3rdChoiceB'],
-	choice3: ['1stChoiceC', '2ndChoiceC', '3rdChoiceC'],
-	choice4: ['1stChoiceD', '2ndChoiceD', '3rdChoiceD'],
-	imageCorrect: [
-		'http://www.simpsoncrazy.com/content/pictures/bart/BartSimpson11.gif',
-		'https://frinkiac.com/img/S15E09/348223/medium.jpg', 
-		'https://frinkiac.com/img/S08E10/940088/medium.jpg'],
-	imageWrong: [],
-//Object function to populate questions and choices
+	question: ['What does the phone number to Moes Tavern spell?', 'Who was the first celebrity to play as themself on The Simpsons?', '3rdQuestion', '4thQuestion', '5thQuestion', '6thQuestion', '7thQuestion', '8thQuestion', '9thQuestion', '10thQuestion'],
+	answer: ['C: S-M-I-T-H-E-R-S', 'B: Tony Bennett', '3rdChoiceA', '4thChoiceC', '5thChoiceD', '6thChoiceA', '7thChoiceC', '8thChoiceD', '9thChoiceA', '10thChoiceD'],
+	choice1: ['A: L-O-V-E D-U-F-F', 'A: Elizabeth Taylor', '3rdChoiceA','4thChoiceA', '5thChoiceA', '6thChoiceA', '7thChoiceA', '8thChoiceA', '9thChoiceA', '10thChoiceA'],
+	choice2: ['B: M-O-E-R-O-C-K-S', 'B: Tony Bennett', '3rdChoiceB','4thChoiceB', '5thChoiceB', '6thChoiceB', '7thChoiceB', '8thChoiceB', '9thChoiceB', '10thChoiceB'],
+	choice3: ['C: S-M-I-T-H-E-R-S', 'C: Mick Jagger', '3rdChoiceC','4thChoiceC', '5thChoiceC', '6thChoiceC', '7thChoiceC', '8thChoiceC', '9thChoiceC', '10thChoiceC'],
+	choice4: ['D: I-M-B-A-R-N-E-Y', 'D: Phil Hartman', '3rdChoiceD','4thChoiceD', '5thChoiceD', '6thChoiceD', '7thChoiceD', '8thChoiceD', '9thChoiceD', '10thChoiceD'],
+
+	imageCorrect: ['assets/images/correct1.gif', 'assets/images/correct2.gif', 'assets/images/correct3.gif', 'assets/images/correct4.gif', 'assets/images/correct5.gif', 
+		'assets/images/correct6.gif', 'assets/images/correct7.gif', 'assets/images/correct8.gif', 'assets/images/correct9.gif', 'assets/images/correct10.gif'],
+	imageWrong: ['assets/images/wrong1.jpg', 'assets/images/wrong2.gif', 'assets/images/wrong3.gif', 'assets/images/wrong4.gif', 'assets/images/wrong5.gif', 
+		'assets/images/wrong6.gif', 'assets/images/wrong7.gif', 'assets/images/wrong8.gif', 'assets/images/wrong9.gif', 'assets/images/wrong10.gif'],
+
  	populate: function() {
  		$("#question").html(this.question[questionindex])
 		$("#choice1").html(this.choice1[questionindex]).attr("data-selection", this.choice1[questionindex]);
@@ -27,20 +27,19 @@ var quiz = {
 		$("#choice4").html(this.choice4[questionindex]).attr("data-selection", this.choice4[questionindex]);
 		}
 	};
-//Countdown Timer Functionality
+
 	function timerStart() {
 		intervalId = setInterval(decrement, 1000);
 	};
 
 	function decrement() {
 		timer--;
-
 		$("#timer").html(timer + "s");
 
 		if (timer === 0) {
 			noAnswer++;
 			timerStop();
-			revealAnswer();
+			revealWrong();
 		}
 	};
 
@@ -48,39 +47,63 @@ var quiz = {
 		clearInterval(intervalId);
 	};
 
-//Onload hides content except for red button to init
+//Onload hides content except for red button to start and loads audio
+$("#warn").hide();
 $(".content").hide();
 $(".reveal").hide();
-
+$("#warning").trigger('load');
+$("#theme").trigger('load');
+$("#wahoo").trigger('load');
+$("#doh").trigger('load');
 
 	$("#init").click( function() {
-		$(".content").show();
+		$("#warning").trigger('play');
 		$("#init").hide();
+		$("#warn").show();
 		$("#timer").html(timer + "s");
-		quiz.populate();
-		timerStart();
+
+		setTimeout(function() {
+			$(".title").hide();
+			$("#warn").hide();
+			$(".content").show();
+			$("#warning").trigger('pause');
+			$("#theme").trigger('play')
+			quiz.populate();
+			timerStart();
+		}, 6000);
 	});
 
 	$(".choices").click( function() {
 		var selection = ($(this).attr("data-selection"));
-		console.log(selection);
+
 		if (selection === quiz.answer[questionindex]) {
 			correct++;
-			revealAnswer();
-			// alert("woohoo!")
+			revealCorrect();
 		} else {
 			wrong++;
-			revealAnswer();
-			// alert("doh!")
+			revealWrong();
 		}
 	});
 
-	function revealAnswer() {
+	function revealCorrect() {
+		$("#wahoo").trigger('play');
 		timerStop();
+		$("#answer").html("WOOHOO! " + quiz.answer[questionindex] + " is the correct answer!");
+		$("#answer-img").html("<img src=" + quiz.imageCorrect[questionindex] + " width='400px'>");
+		revealAnswer();
+	};
+
+	function revealWrong() {
+		$("#doh").trigger('play');
+		timerStop();
+		$("#answer").html("DOH! " + quiz.answer[questionindex] + " was the right answer.");
+		$("#answer-img").html("<img src=" + quiz.imageWrong[questionindex] + " width='400px'>");
+		revealAnswer();
+	};
+
+	function revealAnswer() {
 		$(".content").hide();
 		$(".reveal").show();
-		$("#answer").html("The Answer is: " + quiz.answer[questionindex]);
-		$("#answer-img").html("<img src=" + quiz.imageCorrect[questionindex] + " width='400px'>");
 		questionindex++
 		setTimeout(nextQuestion, 5000);
 	};
@@ -102,14 +125,15 @@ $(".reveal").hide();
 	function result() {
 		$(".reveal").hide();
 		$(".result").show();
-		$("#correct").html("You got " + correct + " correct");
-		$("#wrong").html("You missed " + wrong + " answers");
-		$("#unanswered").html("You left " + noAnswer + " questions unanswered");
+		$("#correct").html("Cowabunga! You got " + correct + " correct!");
+		$("#wrong").html("Oops. You got " + wrong + " wrong :(");
+		$("#unanswered").html("Hmm... You left " + noAnswer + " unanswered.");
 		setTimeout(resetGame, 5000);
 	};
 
 	function resetGame() {
 		timerStop();
+		$("#theme").trigger('pause');
 		timer = 10;
 		questionindex = 0;
 		correct = 0;
@@ -118,7 +142,10 @@ $(".reveal").hide();
 		$(".content").hide();
 		$(".reveal").hide();
 		$(".result").hide();
+		$(".title").show();
 		$("#init").show();
+		$("#theme").trigger('load');
+		$("#warning").trigger('load');
 	};
 
 });
